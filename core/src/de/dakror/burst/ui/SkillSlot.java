bcb522e312b344207b5b1be3bc46bee6f5ca2eb5
Dakror <mail@dakror.de>
2014-10-07 22:46:44 +0200

    pulsating tooltip background

--- core/src/de/dakror/burst/ui/SkillSlot.java
+++ core/src/de/dakror/burst/ui/SkillSlot.java
@@ -1,9 +1,11 @@
 package de.dakror.burst.ui;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.BitmapFont.TextBounds;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -21,6 +23,11 @@ public class SkillSlot extends Button
 {
 	Skill skill;
 	BitmapFont font;
+	
+	final Color c = new Color(1, 1, 1, 1);
+	float delta;
+	
+	static NinePatch bg_fg, bg_bg;
 	
 	public SkillSlot(Skill skill)
 	{
@@ -41,6 +48,21 @@ public class SkillSlot extends Button
 		
 		font = new BitmapFont();
 		font.setMarkupEnabled(true);
+		
+		if (bg_fg == null)
+		{
+			int s = 40;
+			bg_fg = new NinePatch(Burst.img.findRegion("glowBg_fg"), s, s, s, s);
+			bg_bg = new NinePatch(Burst.img.findRegion("glowBg_bg"), s, s, s, s);
+		}
+	}
+	
+	@Override
+	public void act(float delta)
+	{
+		super.act(delta);
+		if (isOver()) this.delta += delta;
+		else this.delta = 0;
 	}
 	
 	@Override
@@ -52,7 +74,14 @@ public class SkillSlot extends Button
 			String str = skill.getParsedDescription(Game.player);
 			int width = 200;
 			TextBounds tb = font.getWrappedBounds(str, width);
-			font.drawWrapped(batch, str, getX() - (tb.width - getWidth()) / 4, getY() + getHeight() + tb.height + 10, width);
+			
+			float x = getX() - (tb.width - getWidth()) / 4;
+			float y = getY() + getHeight() + tb.height + 10;
+			
+			bg_bg.setColor(c.set(1, 1, 1, (float) Math.sin(delta * Math.PI) * 0.4f + 0.6f));
+			bg_bg.draw(batch, x - 40, y - tb.height - 40, width + 80, tb.height + 80);
+			bg_fg.draw(batch, x - 40, y - tb.height - 40, width + 80, tb.height + 80);
+			font.drawWrapped(batch, str, x, y, width);
 		}
 	}
 }
